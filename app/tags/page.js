dd9d97a35d50c90d3b96750daa8fa3c2d9ac3200
kunal-kasture <kunal699704@gmail.com>
2026-06-01 17:54:04 +0530

    Completed Module 2 Milestone 4: Built the Tags UI

--- app/tags/page.js
+++ app/tags/page.js
@@ -1,8 +1,17 @@
-const Tags = () => {
+import TagCard from "../../components/Tag";
+
+const Tags = async () => {
+  const res = await fetch(`https://qevent-backend.labs.crio.do/tags`);
+  const tags = await res.json();
+
   return (
-    <>
-      <h1 className="font-bold text-2xl p-4 m-2">Tags Page</h1>
-    </>
+    <div>
+      <div className="flex flex-wrap w-full pt-24 mt-24 px-6 justify-center gap-6">
+        {tags.map((tag) => (
+          <TagCard id={tag.id} text={tag.name} className="p-4" />
+        ))}
+      </div>
+    </div>
   );
 };
 
